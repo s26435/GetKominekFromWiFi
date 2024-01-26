62cbdee605695c93976c7a01664254059541fd54
s26435 <92215037+s26435@users.noreply.github.com>
2024-01-26 15:41:09 +0100

    Update ESP.ino

--- ESP.ino
+++ ESP.ino
@@ -1,17 +1,89 @@
 #include <SoftwareSerial.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include <Arduino_JSON.h>
 
-SoftwareSerial ESP8266(D2, D3);
+SoftwareSerial ES(D2, D3);
+const char* ssid = "dupa";
+const char* password = "dupadupa";
+const char* serverName = "http://51.68.155.42:5000/info/1";
+
+unsigned long lastTime = 0;
+unsigned long timerDelay = 5000;
+
+String sensorReadings;
+float sensorReadingsArr[3];
 
 void setup(){
+	WiFi.begin(ssid, password);
 	Serial.begin(9600);
-	ESP8266.begin(4800);
+	ES.begin(4800);
 	pinMode(D2,INPUT);
 	pinMode(D4, OUTPUT);
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+float sensorReadingsArr[3];
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+  Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
 }
 
-void loop(){
-	ESP8266.print("10");
-	ESP8266.println("\n");
-	delay(30);
+void loop() {
+  if ((millis() - lastTime) > timerDelay) {
+    if(WiFi.status()== WL_CONNECTED){      
+      sensorReadings = httpGETRequest(serverName);
+      Serial.println(sensorReadings);
+      JSONVar myObject = JSON.parse(sensorReadings);
+      if (JSON.typeof(myObject) == "undefined") {
+        Serial.println("Parsing input failed!");
+        return;
+      }
+      Serial.print("JSON object = ");
+      Serial.println(myObject);
+      JSONVar keys = myObject.keys();
+      for (int i = 0; i < keys.length(); i++) {
+        JSONVar value = myObject[keys[i]];
+        Serial.print(keys[i]);
+        Serial.print(" = ");
+        Serial.println(value);
+        sensorReadingsArr[i] = double(value);
+      }
+      Serial.print("1 = ");
+      Serial.println(sensorReadingsArr[0]);
+      Serial.print("2 = ");
+      Serial.println(sensorReadingsArr[1]);
+      Serial.print("3 = ");
+      Serial.println(sensorReadingsArr[2]);
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
+  }
+}
+
+String httpGETRequest(const char* serverName) {
+  WiFiClient client;
+  HTTPClient http;
+  http.begin(client, serverName);
+  int httpResponseCode = http.GET();
+  String payload = "{}"; 
+  if (httpResponseCode>0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    payload = http.getString();
+  }
+  else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  http.end();
+
+  return payload;
 }
